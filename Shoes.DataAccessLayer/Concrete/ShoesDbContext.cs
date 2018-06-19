2633b262674ae3a968f69d471d08c8b81527263e
yunusunver <yunus.unver1995@gmail.com>
2018-06-19 10:34:07 +0300

    Create ShoesDbContext

---
+++ Shoes.DataAccessLayer/Concrete/ShoesDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Shoes.EntitiesLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoes.DataAccessLayer.Concrete
+{
+    public class ShoesDbContext:DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Carousel> Caraousels { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductSize> ProductSizes { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Size> Sizes{ get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
