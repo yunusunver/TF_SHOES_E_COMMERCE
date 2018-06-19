df65179536643500bd59774c06ccb69d1f0ff056
yunusunver <yunus.unver1995@gmail.com>
2018-06-19 18:23:51 +0300

    Add Abstract and Concrete in BusinessLayer

---
+++ Shoes.BusinessLayer/Abstract/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using Shoes.EntitiesLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shoes.BusinessLayer.Abstract
+{
+    public interface ICategoryService
+    {
+        List<Category> GetAll();
+        List<Category> GetByCategory();
+        void Add(Category category);
+        void Update(Category category);
+        void Delete(int id);
+    }
+}
